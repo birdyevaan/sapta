a8a20953a3405f0dc0c3b0cbf04af413f68910ea
birdyevaan <evaanbirdy@hotmail.com>
2024-09-22 02:25:20 +0530

    fixed home

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import Taskbar from './components/Taskbar';
 import Footer from './components/Footer';
 import Aboutus from './components/Aboutus';
 import Services from './components/Services';
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import Landing from './components/Landing';
+
 function App() {
   return (
     <div className="App">
-      
-      <BrowserRouter>
-      <Taskbar />
-       <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/mission" element={<Aboutus />} />
-      <Route path="/vision" element={<Services />} />
-
-     </Routes> 
-      <Footer />
-      </BrowserRouter>
-      
+      <HashRouter>
+        <Taskbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/mission" element={<Aboutus />} />
+          <Route path="/vision" element={<Services />} />
+        </Routes> 
+        <Footer />
+      </HashRouter>
     </div>
   );
 }
